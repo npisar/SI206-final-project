662aae86094f32591275e4bba98e189f734826f8
npisar <nrpdude.acc@gmail.com>
2024-12-03 14:03:19 -0500

    artifact data done

--- APIs-and-scraping/get_artifact_data.py
+++ APIs-and-scraping/get_artifact_data.py
@@ -8,17 +8,17 @@ This file will use BS4 to scrape through the Genshin Impact Fandom Wiki Page for
 Each entry in the JSON is an individual piece of each artifact set. Each piece is a span with the class "item". So for example, the first two entries are the Initiate's Flower and the Initiate's Feather. The JSON is formatted like this:
 
 {
-"name": { "Artifact Name" } (taken from the "title" attribute of each piece's <a> element)
+    "name": { "Artifact Name" } (str. taken from the "title" attribute of each piece's <a> element)
 
-"artifactURL": { "Artifact URL" } (taken from the "href" attribute of each piece's <a> element. It will combine the base URL https://genshin-impact.fandom.com/ with that value to make a complete link)
+    "artifactURL": { "Artifact URL" } (str. taken from the "href" attribute of each piece's <a> element. It will combine the base URL https://genshin-impact.fandom.com/ with that value to make a complete link)
 
-"artifactSet": { "Artifact Set Name" } (taken from the "name" column in the table, on the same row as the corresponding piece)
+    "artifactSet": { "Artifact Set Name" } (str. taken from the "name" column in the table, on the same row as the corresponding piece)
 
-"setQuality": { "Artifact Set Quality" }  (taken from the "quality" column in the table, on the same row as the corresponding piece.)
+    "maxSetQuality": { "Max Artifact Set Quality" }  (int. taken from the "quality" column in the table, on the same row as the corresponding piece. it takes the max of the star ranges given in the table)
 
-"setBonuses": { "Artifact Set Bonuses" } (taken from the "bonuses" column in the table, on the same row as the corresponding piece. If there are multiple bonuses (like 2 or 4 piece bonuses), those are made to be two unique entries within the setBonuses entry for each piece)
+    "setBonuses": { "Artifact Set Bonuses" } (dict. taken from the "bonuses" column in the table, on the same row as the corresponding piece.)
 
-"setPieceNum": { "Number of Pieces in the Set" } (a count of how many pieces are in the "pieces" column for a given artifact set (row of the table))
+    "setNumPieces": { "Number of Pieces in the Set" } (int. a count of how many pieces are in the "pieces" column for a given artifact set (row of the table))
 }
 
 This JSON is gotten for every piece in the "pieces" column of the table listing the 54 artifact sets in the game. It outputs to a file called artifact-data.json
@@ -39,7 +39,6 @@ def get_artifacts():
     html_file = "APIs-and-scraping/view-source_https___genshin-impact.fandom.com_wiki_Artifact_Sets.html"
     with open(html_file, 'r', encoding='utf-8') as file:
         soup = BeautifulSoup(file, 'html.parser')
-    base_url = "https://genshin-impact.fandom.com"
     
 
     # Find artifact table
@@ -51,7 +50,7 @@ def get_artifacts():
     artifact_data = []
     
     set_quality_pattern = r"\d"
-    bonus_pattern = r"(\d+)\sPiece:\s([^2|4|6|8|10Piece]+)"
+    bonuses_pattern = r"(\d+)\sPiece:\s(.*?)(?=(\d+\sPiece:|$))"
     for row in rows:
         # print(f"row is {row}")
         columns = row.find_all("td")
@@ -60,20 +59,21 @@ def get_artifacts():
     
         # get set name
         artifact_set_name = columns[0].text.strip()
-        print(f"artifact set name is {artifact_set_name}")
+        # print(f"artifact set name is {artifact_set_name}")
         
         # get max set quality
         set_quality = columns[1].text.strip()
         set_quality_matches = re.findall(set_quality_pattern, set_quality)
         int_set_quality_matches = [int(match) for match in set_quality_matches]
         max_set_quality = max(int_set_quality_matches)
-        print(f"max set quality is {max_set_quality}")
+        # print(f"max set quality is {max_set_quality}")
         
         # get bonuses
         bonuses_column = columns[3].text
         print(bonuses_column)
-        bonus_matches = re.findall(bonus_pattern, bonuses_column)
-        bonuses_dict = {int(num): desc.strip() for num, desc in bonus_matches}
+        bonuses_matches = re.findall(bonuses_pattern, bonuses_column)
+        bonuses_dict = {int(num): desc.strip() for num, desc, _ in bonuses_matches}
+        # print(f"bonus dict is {bonuses_dict}")
 
         # get the pieces column and get all pieces
         pieces_column = columns[2]
@@ -85,7 +85,11 @@ def get_artifacts():
             if not link:
                 continue
     
+            # get name
             artifact_name = link.get("title")
+
+            # get url
+            base_url = "https://genshin-impact.fandom.com"
             artifact_url = f"{base_url}{link.get('href')}"
     
             artifact_data.append({
@@ -93,16 +97,15 @@ def get_artifacts():
                 "artifactURL": artifact_url,
                 "artifactSet": artifact_set_name,
                 "maxSetQuality": max_set_quality,
-                "setBonuses": bonus_matches,
+                "setBonuses": bonuses_dict,
                 "setNumPieces": set_num_pieces,
             })
     
     # Export to JSON
     output_file = "artifact-data.json"
     with open(output_file, "w", encoding="utf-8") as json_file:
-        json.dump(artifact_data, json_file, indent=4, ensure_ascii=False)
-    
-    print(f"Data has been written to {output_file}")
+        json.dump(artifact_data, json_file, indent=4)
+    print(f"Data successfully written to {output_file}")
 
 
 def main():
